8107a867cd829ad3641f54765d19bb461271c9c0
npakk <npakk669@gmail.com>
2021-07-22 01:12:34 +0900

    fix:12月の月末が取得出来ない問題に対応

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -7,26 +7,36 @@ require 'optparse'
 Width = 20
 DayWidth = 2
 
-# 日付
+# オプション
 opts = ARGV.getopts('y:', 'm:')
 
+# 日付定義
 today = Date.today
 year = opts["y"] ? opts["y"].to_i : today.year
-month = opts["m"] ? opts["m"].to_i : today.month
-start_of_day = Date.new(year, month, 1)        # 月初
-end_of_day = Date.new(year, month + 1, 1) - 1  # 月末
+month = if opts["m"]
+          raise "#{opts["m"]} is neither a month number (1..12) nor a name" unless (1..12) === opts["m"].to_i
+
+          opts["m"].to_i
+        else
+          today.month
+        end
+start_of_day = Date.new(year, month, 1)          # 月初
+end_of_day = Date.new(year, month, -1)           # 月末
 
 # カレンダー出力
 puts %(#{month}月 #{year}).center(Width)
 puts %(日 月 火 水 木 金 土)
 
 (start_of_day..end_of_day).each_with_object(Array.new) do |date, week|
+
+  # 今日の日付は色を変更
   if date == today
     week.push("\e[31m#{date.day.to_s.rjust(DayWidth)}\e[0m")
   else
     week.push(date.day.to_s.rjust(DayWidth))
   end
 
+  # 土曜日もしくは月末なら改行し、それぞれ左または右に文字寄せ
   case
   when date.saturday?
     puts %(#{week.pop(week.size).join("\s")}).rjust(Width)
