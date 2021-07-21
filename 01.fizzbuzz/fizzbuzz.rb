d5e8124e9a8cbbefad6fdde67b4ee5973a335b2c
npakk <npakk669@gmail.com>
2021-07-21 15:44:51 +0900

    fix magic number

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,14 +1,16 @@
 #!/usr/bin/env ruby
 
+x = {Fizz: 3, Buzz: 5, FizzBuzz: 3*5}
 (1..20).each do |n|
   case
-  when n % (3 * 5) == 0
-    puts "FizzBuzz"
-  when n % 3 == 0
-    puts "Fizz"
-  when n % 5 == 0
-    puts "Buzz"
+  when n % x[:FizzBuzz] == 0
+    puts :FizzBuzz.to_s
+  when n % x[:Fizz] == 0
+    puts :Fizz.to_s
+  when n % x[:Buzz] == 0
+    puts :Buzz.to_s
   else
     puts n
   end
 end
+
