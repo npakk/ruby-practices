aaf6f4d631360875945d6899dcf6c127b8711c76
npakk <npakk669@gmail.com>
2021-07-23 04:57:00 +0900

    fix rubocop offenses

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -4,19 +4,19 @@ require 'date'
 require 'optparse'
 
 # 文字列出力幅
-Width = 20
-DayWidth = 2
+WIDTH = 20
+DAY_WIDTH = 2
 
 # オプション
 opts = ARGV.getopts('y:', 'm:')
 
 # 日付定義
 today = Date.today
-year = opts["y"] ? opts["y"].to_i : today.year
-month = if opts["m"]
-          raise "#{opts["m"]} is neither a month number (1..12) nor a name" unless (1..12) === opts["m"].to_i
+year = opts['y'] ? opts['y'].to_i : today.year
+month = if opts['m']
+          raise "#{opts['m']} is neither a month number (1..12) nor a name" unless (1..12).cover?(opts['m'].to_i)
 
-          opts["m"].to_i
+          opts['m'].to_i
         else
           today.month
         end
@@ -24,24 +24,21 @@ start_of_day = Date.new(year, month, 1)          # 月初
 end_of_day = Date.new(year, month, -1)           # 月末
 
 # カレンダー出力
-puts %(#{month}月 #{year}).center(Width)
+puts %(#{month}月 #{year}).center(WIDTH)
 puts %(日 月 火 水 木 金 土)
 
-(start_of_day..end_of_day).each_with_object(Array.new) do |date, week|
-
+(start_of_day..end_of_day).each_with_object([]) do |date, week|
   # 今日の日付は色を変更
   if date == today
-    week.push("\e[31m#{date.day.to_s.rjust(DayWidth)}\e[0m")
+    week.push("\e[31m#{date.day.to_s.rjust(DAY_WIDTH)}\e[0m")
   else
-    week.push(date.day.to_s.rjust(DayWidth))
+    week.push(date.day.to_s.rjust(DAY_WIDTH))
   end
 
   # 土曜日もしくは月末なら改行し、それぞれ左または右に文字寄せ
-  case
-  when date.saturday?
-    puts %(#{week.pop(week.size).join("\s")}).rjust(Width)
-  when date == end_of_day
-    puts %(#{week.pop(week.size).join("\s")}).ljust(Width)
+  if date.saturday?
+    puts week.pop(week.size).join("\s").to_s.rjust(WIDTH)
+  elsif date == end_of_day
+    puts week.pop(week.size).join("\s").to_s.ljust(WIDTH)
   end
 end
-
