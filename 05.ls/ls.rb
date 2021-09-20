5aa725f1e2a9d67f2430ae2e75c3af0bc90e96db
npakk <npakk669@gmail.com>
2021-09-21 04:15:42 +0900

    修正 名称などをわかりやすいように変更 メソッドの責務を減らすように変更

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -2,67 +2,59 @@
 # frozen_string_literal: true
 
 module LS
-  class Options
+  class Option
     require 'optparse'
 
     ALL = :all
     LONG = :long
     REVERSE = :reverse
 
-    attr_reader :files
-
     def initialize
-      @options = {}
+      @option = {}
       OptionParser.new do |opt|
         opt.on(
           '-a',
           'Include directory entries whose names begin with a dot (.).'
-        ) { |v| @options[ALL] = v }
+        ) { |v| @option[ALL] = v }
         opt.on(
           '-l',
           "(The lowercase letter ``ell''.)  List in long format.  (See below.)  A total sum for all the file sizes is output on a line before the long listing."
-        ) { |v| @options[LONG] = v }
+        ) { |v| @option[LONG] = v }
         opt.on(
           '-r',
           'Reverse the order of the sort to get reverse lexicographical order or the oldest entries first (or largest files last, if combined with sort by size'
-        ) { |v| @options[REVERSE] = v }
+        ) { |v| @option[REVERSE] = v }
 
-        opt.parse!(ARGV)
-
-        break if ARGV.empty?
-
-        @files = specified_files
+        opt.parse(ARGV)
       end
     end
 
-    def has?(name)
-      @options.include?(name)
+    def all?
+      !!@option[ALL]
     end
 
-    def get(name)
-      @options[name]
+    def long?
+      !!@option[LONG]
     end
 
-    def file_match
-      @options[ALL] ? File::FNM_DOTMATCH : 0
+    def reverse?
+      !!@option[REVERSE]
+    end
+  end
+
+  class Argument
+    attr_reader :paths
+
+    def initialize
+      @paths = ARGV.reject { |arg| arg.start_with?('-') }
     end
 
-    private
+    def path_empty?
+      @paths.empty?
+    end
 
-    def specified_files
-      # ファイルを、存在しないもの < ファイル < ディレクトリの順に並べる
-      not_exist = ARGV.reject { |v| File.exist?(v) }
-      files = ARGV.select { |v| File.file?(v) }
-      dirs = ARGV.select { |v| File.directory?(v) }
-
-      not_exist.sort!
-      files.sort! do |a, b|
-        @options[REVERSE] ? b <=> a : a <=> b
-      end
-      @options[REVERSE] ? dirs.reverse! : dirs.sort!
-
-      # ファイル指定されたものかどうか後続処理で区別するため、配列のまま返す
-      [*not_exist, files, *dirs]
+    def path_dir_multiple?
+      @paths.count { |path| FileTest.directory?(path) } > 1
     end
   end
 
@@ -89,43 +81,38 @@ module LS
       '1' => '--x'
     }.freeze
 
-    S_PERMISSIONS = {
+    SPECIAL_PERMISSIONS = {
       '0' => '',
       '1' => 't',
       '2' => 's',
       '4' => 's'
     }.freeze
 
-    def initialize(file)
-      @file = file
-      @file_stat = File.lstat(file)
+    def initialize(path, dir)
+      @file = File.basename(path)
+      @file_stat = File.lstat(dir + path)
     end
 
     def name
       if @file_stat.symlink?
-        "#{File.basename(@file)} -> #{File.readlink(@file)}"
+        "#{@file} -> #{File.readlink(@file)}"
       else
-        File.basename(@file)
+        @file
       end
     end
 
     def mode
       mode = format('%06d', @file_stat.mode.to_s(8))
-      ftype = mode[0..1]
-      s_permission = mode[2]
-      owner = mode[3]
-      group = mode[4]
-      other = mode[5]
+      ftype_number = mode[0..1]
+      special_permission_number = mode[2]
+      owner_number = mode[3]
+      group_number = mode[4]
+      other_number = mode[5]
 
-      ftype = FTYPES[ftype]
-
-      owner = PERMISSIONS[owner]
-      group = PERMISSIONS[group]
-      other = PERMISSIONS[other]
-
-      other = s_permission != '1' ? other : format_special_permission(other, S_PERMISSIONS[s_permission])
-      owner = s_permission != '2' ? owner : format_special_permission(owner, S_PERMISSIONS[s_permission])
-      group = s_permission != '4' ? group : format_special_permission(group, S_PERMISSIONS[s_permission])
+      ftype = FTYPES[ftype_number]
+      owner = get_permission_or_special(special_permission_number == '2', owner_number, special_permission_number)
+      group = get_permission_or_special(special_permission_number == '4', group_number, special_permission_number)
+      other = get_permission_or_special(special_permission_number == '1', other_number, special_permission_number)
 
       ftype + owner + group + other
     end
@@ -149,7 +136,8 @@ module LS
     def time
       month = @file_stat.mtime.month
       day = @file_stat.mtime.day
-      "#{month.to_s.rjust(2)} #{day.to_s.rjust(2)} #{@file_stat.mtime.strftime('%H:%M')}"
+      time = @file_stat.mtime.strftime('%H:%M')
+      "#{month.to_s.rjust(2)} #{day.to_s.rjust(2)} #{time}"
     end
 
     def blocks
@@ -158,8 +146,15 @@ module LS
 
     private
 
-    def format_special_permission(value, s_permission)
-      value[2] == 'x' ? value[0..1] + s_permission : value[0..1] + s_permission.upcase
+    def get_permission_or_special(is_special, authority_number, special_permission_number)
+      permission = PERMISSIONS[authority_number]
+
+      if is_special
+        special_permission = SPECIAL_PERMISSIONS[special_permission_number]
+        special_permission.upcase unless permission[2] == 'x'
+      else
+        permission
+      end
     end
   end
 
@@ -168,82 +163,120 @@ module LS
       HORIZONTAL_COLUMN = 3
 
       def run
-        options = Options.new
-        files = options.files.nil? ? ['./'] : delete_no_such_file(options.files)
+        option = Option.new
+        argument = Argument.new
 
-        files.each do |file|
-          files = if file.instance_of?(Array)
-                    file
-                  else
-                    # ディレクトリを明示されないかぎり、ディレクトリ名を出力しない
-                    puts "\n#{file}:" unless options.files.nil?
-                    glob = Dir.glob('*', options.file_match, base: file)
-                    options.get(Options::REVERSE) ? glob.reverse : glob
-                  end
+        output_not_existance_path(argument.paths)
+        files, dirs = get_existance_paths(argument.paths, option.reverse?)
 
-          options.has?(Options::LONG) ? vertical_formatter(files, file) : horizontal_formatter(files)
-        end
-      end
-
-      def vertical_formatter(files, file)
-        file_stat = { mode: [], nlink: [], user: [], group: [], size: [], time: [], name: [], blocks: [] }
-        files.each do |v|
-          s = LS::FileStatus.new("#{file.instance_of?(Array) ? './' : file}#{v}")
-          file_stat[:mode] << s.mode
-          file_stat[:nlink] << s.nlink
-          file_stat[:user] << s.user
-          file_stat[:group] << s.group
-          file_stat[:size] << s.size
-          file_stat[:time] << s.time
-          file_stat[:name] << s.name
-          file_stat[:blocks] << s.blocks
-        end
-
-        # ファイル指定されたものは、ブロックサイズを表示しない
-        puts "total #{file_stat[:blocks].sum}" unless file.instance_of?(Array)
-
-        w_nlink, w_user, w_group, w_size = *get_max_length_file_stat(file_stat)
-
-        file_stat[:mode].each_with_index do |v, i|
-          puts "#{v}  #{file_stat[:nlink][i].rjust(w_nlink)} #{file_stat[:user][i].rjust(w_user)}  #{file_stat[:group][i].rjust(w_group)}  " \
-          "#{file_stat[:size][i].rjust(w_size)} #{file_stat[:time][i]} #{file_stat[:name][i]}"
-        end
-      end
-
-      def horizontal_formatter(files)
-        files << '' while files.size % HORIZONTAL_COLUMN != 0
-        files = files.each_slice(files.size / HORIZONTAL_COLUMN).to_a
-        # 列ごとのファイル名長のギャップを空白で埋める
-        files.map! do |i|
-          max_length = i.max_by(&:length).size
-          i.map do |j|
-            j.ljust(max_length)
-          end
-        end
-        files.transpose.each do |line|
-          puts line.join("\t\t")
-        end
+        output_files(files, option)
+        output_dirs(dirs, option, argument)
       end
 
       private
 
-      def delete_no_such_file(files)
-        no_such_files = files.select { |v| v.instance_of?(String) && !File.exist?(v) }
-        no_such_files.each { |v| puts "ls: #{v}: No such file or directory" }
-        files - no_such_files
+      def output_not_existance_path(paths)
+        paths.sort!.each do |path|
+          puts "ls: #{path}: No such file or directory" unless FileTest.exist?(path)
+        end
       end
 
-      def get_max_length_file_stat(value)
-        length = []
-        length << get_max_length(value[:nlink])
-        length << get_max_length(value[:user])
-        length << get_max_length(value[:group])
-        length << get_max_length(value[:size])
-        length
+      def get_existance_paths(paths, is_reverse)
+        # 本家lsコマンドと合わせるため、パスをファイル < ディレクトリの順に並べ、その中でsortを行う
+        files = paths.select { |path| FileTest.file?(path) }
+        dirs = paths.select { |path| FileTest.directory?(path) }
+
+        files.sort! do |a, b|
+          is_reverse ? b <=> a : a <=> b
+        end
+        is_reverse ? dirs.reverse! : dirs.sort!
+
+        [files, dirs]
       end
 
-      def get_max_length(value)
-        value.map!(&:to_s).max_by(&:length).size
+      def glob_dir(dir, option)
+        files = Dir.glob('*', option.all? ? File::FNM_DOTMATCH : 0, base: dir)
+
+        if option.reverse?
+          files.reverse
+        else
+          files
+        end
+      end
+
+      def get_status(files, dir = '')
+        files.map do |file|
+          LS::FileStatus.new(file, dir)
+        end
+      end
+
+      def output_dirs(dirs, option, argument)
+        dirs << './' if argument.path_empty?
+        dirs.each do |dir|
+          output_dirname(dir) if argument.path_dir_multiple?
+
+          files = glob_dir(dir, option)
+
+          if option.long?
+            files_status = get_status(files, dir)
+            output_vertical(files_status, is_total_output: true)
+          else
+            output_horizontal(files)
+          end
+        end
+      end
+
+      def output_files(files, option)
+        return if files.empty?
+
+        if option.long?
+          files_status = get_status(files)
+          output_vertical(files_status)
+        else
+          output_horizontal(files)
+        end
+      end
+
+      def output_dirname(dir)
+        puts "\n#{dir}:"
+      end
+
+      def output_vertical(files_status, is_total_output: false)
+        puts "total #{files_status.sum(&:blocks)}" if is_total_output
+
+        max_length_nlink = get_max_length(files_status.map(&:nlink))
+        max_length_user = get_max_length(files_status.map(&:user))
+        max_length_group = get_max_length(files_status.map(&:group))
+        max_length_size = get_max_length(files_status.map(&:size))
+
+        files_status.each do |file_status|
+          mode = file_status.mode.to_s
+          nlink = file_status.nlink.to_s.rjust(max_length_nlink)
+          user = file_status.user.rjust(max_length_user)
+          group = file_status.group.rjust(max_length_group)
+          size = file_status.size.to_s.rjust(max_length_size)
+          time = file_status.time
+          name = file_status.name
+
+          puts "#{mode}  #{nlink} #{user}  #{group}  #{size} #{time} #{name}"
+        end
+      end
+
+      def output_horizontal(files)
+        max_length = get_max_length(files)
+
+        files << '' while files.size % HORIZONTAL_COLUMN != 0
+        row_count = files.size / HORIZONTAL_COLUMN
+        sliced_files = files.each_slice(row_count).to_a
+
+        sliced_files.transpose.each do |row|
+          output_lists = row.map { |file| file.ljust(max_length) }
+          puts output_lists.join("\t")
+        end
+      end
+
+      def get_max_length(list)
+        list.map(&:to_s).max_by(&:length).length
       end
     end
   end
