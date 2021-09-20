4e26eac80c5a43183eabddf75c1248ae1bebe7a1
npakk <npakk669@gmail.com>
2021-09-20 18:47:28 +0900

    修正 Optionクラスを廃止 メソッドの引数を呼出元で整理するように変更 totalの出力条件が間違っていた箇所を修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -2,93 +2,103 @@
 # frozen_string_literal: true
 
 module WC
-  class Option
-    require 'optparse'
-
-    def initialize
-      @option = {}
-      OptionParser.new do |opt|
-        opt.on(
-          '-l',
-          'The number of lines in each input file is written to the standard output.'
-        ) { |v| @option[:lines] = v }
-
-        opt.parse!(ARGV)
-      end
-    end
-
-    def get(opt_name)
-      @option[opt_name]
-    end
-  end
-
-  class StringStatus
+  class TextStatus
     attr_reader :filename
 
-    def initialize(text)
-      @text = if FileTest.file?(text)
-                @filename = text
-                File.read(text)
-              elsif ARGV.empty? && !$stdin.tty?
-                text
-              else
-                ''
-              end
+    def initialize(text, filename = '', is_file: false, is_directory: false)
+      @text = text
+      @filename = filename
+      @is_file = is_file
+      @is_directory = is_directory
     end
 
-    def linecount
+    def line_count
       @text.lines.count
     end
 
-    def wordcount
+    def word_count
       @text.split(/\s+/).size
     end
 
     def bytesize
       @text.bytesize
     end
+
+    def file?
+      @is_file
+    end
+
+    def directory?
+      @is_directory
+    end
   end
 
   class Command
+    require 'optparse'
+
     class << self
       def run
-        @option = Option.new
-        @args = ARGV
+        filenames = parse_option_and_arguments!
 
-        if @args.empty?
-          stdin_status = WC::StringStatus.new($stdin.readlines.join)
-          max_length = get_max_length(stdin_status)
-          output(stdin_status, max_length)
+        if filenames.empty?
+          output_stdin
         else
-          file_status_list = get_file_status_list
-          max_length = get_max_length(file_status_list)
-
-          @args.each do |arg|
-            next puts "wc: #{arg}: open: No such file or directory" unless File.exist?(arg)
-            next puts "wc: #{arg}: read: Is a directory" if File.directory?(arg)
-
-            file_status = file_status_list.find { |status| status.filename == arg }
-            output(file_status, max_length, filename: file_status.filename)
-          end
-
-          # total
-          output(file_status_list, max_length, filename: 'total') if output_total?
+          output_files(filenames)
         end
       end
 
       private
 
-      def get_file_status_list
-        @args.map do |arg|
-          WC::StringStatus.new(arg)
+      def output_stdin
+        text_status = WC::TextStatus.new($stdin.readlines.join)
+        max_counts = get_max_counts([text_status.line_count, text_status.word_count, text_status.bytesize])
+        max_length = get_max_length(max_counts)
+        output(text_status.line_count, text_status.word_count, text_status.bytesize, max_length)
+      end
+
+      def output_files(filenames)
+        text_status_list = get_text_status_list(filenames)
+        max_counts = get_max_counts(text_status_list)
+        max_length = get_max_length(max_counts)
+
+        text_status_list.each do |status|
+          next puts "wc: #{status.filename}: open: No such file or directory" if !status.file? && !status.directory?
+          next puts "wc: #{status.filename}: read: Is a directory" if status.directory?
+
+          output(status.line_count, status.word_count, status.bytesize, max_length, filename: status.filename)
+        end
+
+        line_count, word_count, bytesize = get_status_sum(text_status_list)
+        output(line_count, word_count, bytesize, max_length, filename: 'total') if output_total?(text_status_list)
+      end
+
+      def parse_option_and_arguments!
+        @option = {}
+        OptionParser.new do |opt|
+          opt.on(
+            '-l',
+            'The number of lines in each input file is written to the standard output.'
+          ) { |v| @option[:lines] = v }
+
+          opt.parse!(ARGV)
+        end
+        ARGV
+      end
+
+      def get_text_status_list(filenames)
+        filenames.map do |filename|
+          if FileTest.file?(filename)
+            WC::TextStatus.new(File.read(filename), filename, is_file: true)
+          else
+            is_directory = FileTest.directory?(filename)
+            WC::TextStatus.new('', filename, is_file: false, is_directory: is_directory)
+          end
         end
       end
 
-      def output(status_list, length, filename: '')
-        line, word, size = get_total_status(status_list, sum: true)
-
-        text = format_value(line, length)
-        text += format_value(word, length) + format_value(size, length) unless @option.get(:lines)
+      def output(line_count, word_count, bytesize, length, filename: '')
+        text = format_value(line_count, length)
+        text += format_value(word_count, length) + format_value(bytesize, length) unless @option[:lines]
         text += "\s#{filename}" unless filename.empty?
         puts text
       end
@@ -97,34 +107,27 @@ module WC
         value.to_s.rjust(max_length)
       end
 
-      def get_max_length(status_list)
-        valiables = get_total_status(status_list, sum: false)
-
-        # 本家wcコマンド同様に4文字分の余白を設ける
-        valiables.max.to_s.length + 4
+      def get_max_counts(status_or_status_list)
+        get_status_max(status_or_status_list)
       end
 
-      def get_total_status(status_list, sum:)
-        unless status_list.instance_of?(Array)
-          return [status_list.linecount,
-                  status_list.wordcount,
-                  status_list.bytesize]
-        end
-
-        if sum
-          [status_list.sum(&:linecount),
-           status_list.sum(&:wordcount),
-           status_list.sum(&:bytesize)]
-        else
-          # linecount,wordcount,bytesizeそれぞれのmaxを取得してから、全体でみたときのmaxを取得する
-          [status_list.max_by(&:linecount).linecount,
-           status_list.max_by(&:wordcount).wordcount,
-           status_list.max_by(&:bytesize).bytesize]
-        end
+      def get_max_length(max_counts)
+        # 本家wcコマンド同様に4文字分の余白を設け、最低8文字の長さにする
+        [max_counts.max.to_s.length + 4, 8].max
       end
 
-      def output_total?
-        @args.find { |arg| FileTest.file?(arg) }
+      def get_status_sum(status_list)
+        [status_list.sum(&:line_count), status_list.sum(&:word_count), status_list.sum(&:bytesize)]
+      end
+
+      def get_status_max(status_list)
+        [status_list.max_by(&:line_count).line_count,
+         status_list.max_by(&:word_count).word_count,
+         status_list.max_by(&:bytesize).bytesize]
+      end
+
+      def output_total?(text_status_list)
+        text_status_list.count(&:file?) > 1
       end
     end
   end
