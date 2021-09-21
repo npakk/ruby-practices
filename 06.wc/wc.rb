8dce9286066d47b6fd10e5960264ddc932280a96
npakk <npakk669@gmail.com>
2021-09-21 21:35:15 +0900

    修正 オプションとコマンド引数を分けて取得 配列を期待するメソッドに別の型を渡していたエラーを修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -33,56 +33,59 @@ module WC
     end
   end
 
+  OutputData = Struct.new(:line_count, :word_count, :bytesize, :name)
+
   class Command
     require 'optparse'
 
     class << self
       def run
-        filenames = parse_option_and_arguments!
+        option, filenames = parse_option_and_arguments
 
         if filenames.empty?
-          output_stdin
+          output_stdin(option)
         else
-          output_files(filenames)
+          output_files(filenames, option)
         end
       end
 
       private
 
-      def output_stdin
+      def output_stdin(option)
         text_status = WC::TextStatus.new($stdin.readlines.join)
-        max_counts = get_max_counts([text_status.line_count, text_status.word_count, text_status.bytesize])
-        max_length = get_max_length(max_counts)
-        output(text_status.line_count, text_status.word_count, text_status.bytesize, max_length)
+        max_length = get_max_length([text_status.line_count, text_status.word_count, text_status.bytesize])
+        output_data = OutputData.new(text_status.line_count, text_status.word_count, text_status.bytesize)
+        output(output_data, max_length, option)
       end
 
-      def output_files(filenames)
+      def output_files(filenames, option)
         text_status_list = get_text_status_list(filenames)
-        max_counts = get_max_counts(text_status_list)
-        max_length = get_max_length(max_counts)
+        max_length = get_max_length_for_list(text_status_list)
 
         text_status_list.each do |status|
           next puts "wc: #{status.filename}: open: No such file or directory" if !status.file? && !status.directory?
           next puts "wc: #{status.filename}: read: Is a directory" if status.directory?
 
-          output(status.line_count, status.word_count, status.bytesize, max_length, filename: status.filename)
+          output_data = OutputData.new(status.line_count, status.word_count, status.bytesize, status.filename)
+          output(output_data, max_length, option)
         end
 
         line_count, word_count, bytesize = get_status_sum(text_status_list)
-        output(line_count, word_count, bytesize, max_length, filename: 'total') if output_total?(text_status_list)
+        output_data = OutputData.new(line_count, word_count, bytesize, 'total')
+        output(output_data, max_length, option) if output_total?(text_status_list)
       end
 
-      def parse_option_and_arguments!
-        @option = {}
+      def parse_option_and_arguments
+        option = {}
         OptionParser.new do |opt|
           opt.on(
             '-l',
             'The number of lines in each input file is written to the standard output.'
-          ) { |v| @option[:lines] = v }
+          ) { |v| option[:lines] = v }
 
           opt.parse!(ARGV)
         end
-        ARGV
+        [option, ARGV]
       end
 
       def get_text_status_list(filenames)
@@ -96,10 +99,11 @@ module WC
         end
       end
 
-      def output(line_count, word_count, bytesize, length, filename: '')
-        text = format_value(line_count, length)
-        text += format_value(word_count, length) + format_value(bytesize, length) unless @option[:lines]
-        text += "\s#{filename}" unless filename.empty?
+      def output(output_data, length, option)
+        # Rubocopにより引数の個数5以下を求められたため、Structを採用
+        text = format_value(output_data.line_count, length)
+        text += format_value(output_data.word_count, length) + format_value(output_data.bytesize, length) unless option[:lines]
+        text += "\s#{output_data.name}" unless output_data.name.nil?
         puts text
       end
 
@@ -107,23 +111,21 @@ module WC
         value.to_s.rjust(max_length)
       end
 
-      def get_max_counts(status_or_status_list)
-        get_status_max(status_or_status_list)
+      def get_max_length(max_status_list)
+        # 本家wcコマンド同様に4文字分の余白を設け、最低8文字の長さにする
+        [max_status_list.max.to_s.length + 4, 8].max
       end
 
-      def get_max_length(max_counts)
-        # 本家wcコマンド同様に4文字分の余白を設け、最低8文字の長さにする
-        [max_counts.max.to_s.length + 4, 8].max
+      def get_max_length_for_list(status_list)
+        max_status_list = [status_list.max_by(&:line_count).line_count,
+                           status_list.max_by(&:word_count).word_count,
+                           status_list.max_by(&:bytesize).bytesize]
+
+        get_max_length(max_status_list)
       end
 
       def get_status_sum(status_list)
         [status_list.sum(&:line_count), status_list.sum(&:word_count), status_list.sum(&:bytesize)]
-      end
-
-      def get_status_max(status_list)
-        [status_list.max_by(&:line_count).line_count,
-         status_list.max_by(&:word_count).word_count,
-         status_list.max_by(&:bytesize).bytesize]
       end
 
       def output_total?(text_status_list)
